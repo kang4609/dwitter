f0206b5f7c0d6e466f20b017b6889418ed52f2bd
kang4609 <kang4609@gmail.com>
2021-11-27 22:26:40 +0900

    node validation

--- server/router/tweets.js
+++ server/router/tweets.js
@@ -1,24 +1,45 @@
 import express from 'express';
 import 'express-async-errors';
-
+import { body, param, query } from 'express-validator';
 import * as tweetController from '../controller/tweet.js';
+import { validate } from '../middleware/validator.js';
 
 const router = express.Router();
+
+// validation
+// sanitization
+const validateTweet = [
+    body('text')
+        .trim()
+        .isLength({ min: 3 })
+        .withMessage('text should be at least 3 characters'),
+    validate,
+];
 
 //GET /tweets
 
 //GET /tweets?username=:username
+router.get(
+    '/',
+    [
+        query('username')
+            .trim()
+            .isLength({ min: 2 })
+            .withMessage('이름은 두글자 이상!'),
+        validate,
+    ],
 
-router.get('/', tweetController.getTweets);
+    tweetController.getTweets,
+);
 
 //GET /tweets/:id
 router.get('/:id', tweetController.getTweet);
 
 // POST /tweets
-router.post('/', tweetController.createTweet);
+router.post('/', validateTweet, tweetController.createTweet);
 
 // PUT /tweets/:id
-router.put('/:id', tweetController.updateTweet);
+router.put('/:id', validateTweet, tweetController.updateTweet);
 // DELETE /tweets/:id
 router.delete('/:id', tweetController.deleteTweet);
 
